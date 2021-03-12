ffc7569c352db44830ea11bed79eeb377d36220a
plan5 <plan5@posteo.de>
2021-03-12 23:32:50 +0100

    Add cija

---
+++ cija_32x32_expansion_for_Pipoya_CC0_1152x1280.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="cija_32x32_expansion_for_Pipoya_CC0_1152x1280" tilewidth="32" tileheight="32" tilecount="1440" columns="36">
+ <image source="../../digitalegesellschaft/workadventure-map-winterkongress/tiles_png/community/bytewerk/cija_32x32_expansion_for_Pipoya_CC0_1152x1280.png" width="1152" height="1280"/>
+</tileset>
