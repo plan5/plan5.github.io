5e0023a5e791d3a3a5e1599cab69b6c4cfda9168
plan5 <plan5@posteo.de>
2021-03-12 19:36:56 +0100

    Fix Platz tiles gaps by removing 2px spacing in tileset

---
+++ roguelikeCity_transparent_32x32_nospace.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="roguelikeCity_transparent_32x32_nospace" tilewidth="32" tileheight="32" tilecount="1036" columns="37">
+ <image source="roguelikeCity_transparent_32x32_nospace.png" width="1184" height="896"/>
+</tileset>
